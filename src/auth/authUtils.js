c140015af2dcf8c675c9b9217697aeea1ab7db1d
letronghiep <letronghiep213@gmail.com>
2024-06-27 10:15:22 +0700

    logout & authentication

--- src/auth/authUtils.js
+++ src/auth/authUtils.js
@@ -1,5 +1,18 @@
 "use strict";
 const JWT = require("jsonwebtoken");
+const KeyTokenService = require("../services/keyToken.service");
+const {
+  NotFoundError,
+  BadRequestError,
+  AuthFailureError,
+} = require("../core/error.response");
+const { asyncHandler } = require("../helpers/asyncHandler");
+const HEADER = {
+  API_KEY: "x-api-key",
+  CLIENT_ID: "x-client-id",
+  AUTHORIZATION: "authorization",
+  REFRESH_TOKEN: "x-rToken-id",
+};
 const createTokensPair = async (payload, publicKey, privateKey) => {
   try {
     const accessToken = await JWT.sign(payload, publicKey, {
@@ -21,6 +34,39 @@ const createTokensPair = async (payload, publicKey, privateKey) => {
     console.error(error);
   }
 };
+
+const authentication = asyncHandler(async (req, res, next) => {
+  /**
+   * - Check userId missing??
+   * get accessToken
+   * verifyToken
+   * check user in db
+   * check keyStore with userId
+   * Ok all => return next()
+   *  */
+  const userId = req.headers[HEADER.CLIENT_ID];
+  if (!userId) throw new AuthFailureError("Invalid Request");
+  const keyStore = await KeyTokenService.findUserById(userId);
+  if (!keyStore) throw new NotFoundError("Key store is not found");
+  const accessToken = req.headers[HEADER.AUTHORIZATION];
+  if (!accessToken) throw new AuthFailureError("Invalid Request");
+  try {
+    const decodeUser = JWT.verify(accessToken, keyStore.publicKey);
+    if (userId !== decodeUser.userId)
+      throw new AuthFailureError("Invalid User id");
+
+    req.keyStore = keyStore;
+    req.user = decodeUser;
+    return next();
+  } catch (error) {
+    throw error;
+  }
+});
+const verifyJWT = async (token, keySecret) => {
+  return await JWT.verify(token, keySecret);
+};
 module.exports = {
   createTokensPair,
+  authentication,
+  verifyJWT,
 };
--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -4,13 +4,25 @@ const { CREATED, SuccessResponse } = require("../core/success.response");
 const AccessService = require("../services/access.service");
 
 class AccessController {
+  handleRefreshToken = async (req, res) => {
+    new SuccessResponse({
+      message: "Refresh token is handler",
+      metadata: await AccessService.handleRefreshToken(req.body.refreshToken),
+    }).send(res);
+  };
+  logout = async (req, res) => {
+    new SuccessResponse({
+      message: "Logout Success",
+      metadata: await AccessService.logout(req.keyStore),
+    }).send(res);
+  };
   login = async (req, res) => {
     new SuccessResponse({
       message: "Login success",
 
-      metadata: await AccessService.login(req.body)
+      metadata: await AccessService.login(req.body),
     }).send(res);
-  }
+  };
   signUp = async (req, res, next) => {
     new CREATED({
       message: "Shop register success",
